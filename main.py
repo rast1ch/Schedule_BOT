2bc5de6c3aef9e9c184150a06b04849e74ad804e
rast1ch <rast1ch2286@gmail.com>
2021-04-07 17:09:18 -0400

    Involved schedule-change function

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ schedule = service.spreadsheets().values().get(
         range = "A1:C500",
         majorDimension = 'ROWS').execute()['values']
 day_schedule = []
-
+data = 0
+callback = []
 #-------------------------------------------------------------
 admin_flag = False
 admin_code = '123'
 admin_id = ''
 admin_message = ''
-
 
 
 #-------------------------MARKUPS-----------------------------
@@ -55,6 +55,16 @@ schedule_change_thur = telebot.types.InlineKeyboardMarkup()
 schedule_change_thur.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair4'))
 schedule_change_fri = telebot.types.InlineKeyboardMarkup()
 schedule_change_fri.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair5'))
+schedule_change_mon_1 = telebot.types.InlineKeyboardMarkup()
+schedule_change_mon_1.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair6'))
+schedule_change_tues_2 = telebot.types.InlineKeyboardMarkup()
+schedule_change_tues_2.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair7'))
+schedule_change_wed_3 = telebot.types.InlineKeyboardMarkup()
+schedule_change_wed_3.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair8'))
+schedule_change_thur_4 = telebot.types.InlineKeyboardMarkup()
+schedule_change_thur_4.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair9'))
+schedule_change_fri_5 = telebot.types.InlineKeyboardMarkup()
+schedule_change_fri_5.add(telebot.types.InlineKeyboardButton(text = "Изменить Пару", callback_data = 'change_pair10'))
 # --------------------CREATING_BOT----------------------------
 bot = telebot.TeleBot('1795577258:AAGOp6HC5mfZfwQ5vd6jL5LxkHN-Py-pxiI')
 
@@ -102,55 +112,37 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_data(call):
+    global callback, data
     if call.data == 'admin_creategroup':
         msg = bot.send_message(call.message.chat.id, "Введите номер(название группы)")
         bot.register_next_step_handler(msg, create_group)
     elif call.data == 'admin_chosegroup':
         msg = bot.send_message(call.message.chat.id, "Введите номер(название группы)")
         bot.register_next_step_handler(msg, chose_group)
+    elif call.data == 'change_pair1':
+        callback = [data, 1]
+    elif call.data == 'change_pair2':
+        callback = [data, 2]
+    elif call.data == 'change_pair3':
+        callback = [data, 3]
+    elif call.data == 'change_pair4':
+        callback = [data, 4]
+    elif call.data == 'change_pair5':
+        callback = [data, 5]
+    elif call.data == 'change_pair6':
+        callback = [data, 6]
+    elif call.data == 'change_pair7':
+        callback = [data, 7]
+    elif call.data == 'change_pair8':
+        callback = [data, 8]
+    elif call.data == 'change_pair9':
+        callback = [data, 9]
+    elif call.data == 'change_pair10':
+        callback = [data, 10]
+    if callback:
+        msg = bot.send_message(call.message.chat.id, "Введите Предметы через запятую для заполнения. В случае окна, поставьте пробел и запятую")
+        bot.register_next_step_handler(msg, change)
 
-
-def chose_group(message):
-    global groups, admin_message, schedule
-    i = 0
-    j = 0
-    while i < len(groups):
-        if groups[i] == message.text:
-            data = message.text
-            if len(schedule[i])<3:
-                while j<2:
-                    schedule[i].append(" , , , , , , , , , , , , , , , , , , , ")
-                    j+=1
-                service.spreadsheets().values().batchUpdate(
-                    spreadsheetId = spreadsheet_id,
-                    body = {
-                    'valueInputOption':'USER_ENTERED',
-                    'data': [
-                    {'range': "A1:C500",
-                    'majorDimension' : 'ROWS',
-                    'values': schedule}]}).execute()
-                j=0
-            bot.send_message(message.from_user.id, f"Группа номер {message.text}")
-            if schedule[i][1] == " , , , , , , , , , , , , , , , , , , , ":
-                bot.send_message(message.from_user.id, "На числитель пока расписания нет")
-                if schedule[i][2] == " , , , , , , , , , , , , , , , , , , , ":
-                    bot.send_message(message.from_user.id, "На знаменатель пока расписания нет")
-            else:
-                k =0
-                l =0
-                days = ["Понедельник","Вторник","Среда","Четверг","Пятница"]
-                markups = [schedule_change_mon, schedule_change_tues,schedule_change_wed,schedule_change_thur,schedule_change_fri]
-                for x in schedule[i][1:]:
-                    day_schedule.append(x.split(','))
-                for x in days:
-                    bot.send_message(message.from_user.id, f"{x}\n\t1) {day_schedule[0][k]}\n\t2) {day_schedule[0][k+1]}\n\t3) {day_schedule[0][k+2]}\n\t4) {day_schedule[0][k+3]}",reply_markup = markups[k//4])
-                    k+=4
-
-
-            break
-        i+=1
-    if message.text not in groups:
-        bot.send_message(message.from_user.id, "Проверьте правильность ввода номера группы")
 
 def create_group(message):
     global groups, admin_message
@@ -172,6 +164,96 @@ def create_group(message):
     bot.register_next_step_handler(admin_message, admin_panel)
 
 
+def chose_group(message):
+    global groups, admin_message, schedule, day_schedule, data
+    day_schedule = []
+    i = 0
+    j = 0
+    while i < len(groups):
+        if groups[i] == message.text:
+            data = i
+            if len(schedule[i])<3:
+                while j<2:
+                    schedule[i].append(' , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ')
+                    j+=1
+                service.spreadsheets().values().batchUpdate(
+                    spreadsheetId = spreadsheet_id,
+                    body = {
+                    'valueInputOption':'USER_ENTERED',
+                    'data': [
+                    {'range': "A1:C500",
+                    'majorDimension' : 'ROWS',
+                    'values': schedule}]}).execute()
+                j=0
+            bot.send_message(message.from_user.id, f"Группа номер {message.text}")
+            #if schedule[i][1] == ' , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ':
+                #bot.send_message(message.from_user.id, "На числитель пока расписания нет")
+                #if schedule[i][2] == ' , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ':
+                    #bot.send_message(message.from_user.id, "На знаменатель пока расписания нет")
+            #else:
+            k =0
+            l =0
+            days = ["Понедельник","Вторник","Среда","Четверг","Пятница"]
+            markups = [schedule_change_mon, schedule_change_tues,schedule_change_wed,schedule_change_thur,schedule_change_fri,schedule_change_mon_1, schedule_change_tues_2,schedule_change_wed_3,schedule_change_thur_4,schedule_change_fri_5]
+            for x in schedule[i][1:]:
+                day_schedule.append(x.split(','))
+            bot.send_message(message.from_user.id, "Рассписание на числитель")
+            for x in days:
+                bot.send_message(message.from_user.id, f"{x}\n\t1) {day_schedule[0][k]}\n\t2) {day_schedule[0][k+1]}\n\t3) {day_schedule[0][k+2]}\n\t4) {day_schedule[0][k+3]}\n\t5) {day_schedule[0][k+4]}\n\t6) {day_schedule[0][k+5]}\n\t7) {day_schedule[0][k+6]}\n\t8) {day_schedule[0][k+7]}",reply_markup = markups[l])
+                k+=8
+                l+=1
+            k = 0
+            bot.send_message(message.from_user.id, "Рассписание на знаментель")
+            print(len(day_schedule[0]))
+            print(len(markups))
+            for x in days:
+                bot.send_message(message.from_user.id, f"{x}\n\t1) {day_schedule[1][k]}\n\t2) {day_schedule[1][k+1]}\n\t3) {day_schedule[1][k+2]}\n\t4) {day_schedule[1][k+3]}\n\t5) {day_schedule[1][k+4]}\n\t6) {day_schedule[1][k+5]}\n\t7) {day_schedule[1][k+6]}\n\t8) {day_schedule[1][k+7]}",reply_markup = markups[l])
+                k+=8
+                l+=1
+            k = 0 
+            l = 0   
+            break
+        i+=1
+    i=0
+    if message.text not in groups:
+        bot.send_message(message.from_user.id, "Проверьте правильность ввода номера группы")
+
+
+def change(message):
+    global callback, schedule, day_schedule, data
+    messg_temp = message.text.strip(" ")
+    messg_temp = messg_temp.split(',')
+    print(messg_temp)
+    if len(messg_temp)>8:
+        bot.send_message("Слишком много уроков для 1 дня")
+    else:
+        while len(messg_temp)< 8:
+            messg_temp.append(" ")
+        if callback[1]>5:
+            indexs = (callback[1]- 6)*8
+        else:
+            indexs = (callback[1]-1)*8
+        print(messg_temp)
+        day_schedule[callback[1]//6][indexs:indexs + 8]=messg_temp
+        print(day_schedule[callback[1]//6][indexs:indexs + 8])
+        print(" ,".join(day_schedule[callback[1]//6]))
+        schedule[callback[0]][callback[1]//6+1] = " ,".join(day_schedule[callback[1]//6])
+        service.spreadsheets().values().batchUpdate(
+            spreadsheetId = spreadsheet_id,
+            body = {
+            'valueInputOption':'USER_ENTERED',
+            'data': [
+            {'range': "A1:C500",
+            'majorDimension' : 'ROWS',
+            'values': schedule}]}).execute()
+        day_schedule= []
+        callback =[]
+        data = 0
+        bot.send_message(message.from_user.id, "Изменения успешно внесены")
+        
+
+
+    
 
 
 
